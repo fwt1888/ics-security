c4b94f8e6f48b82a5a1be03952666ae293973bec
fwt1888 <fwt1888@gmail.com>
2024-09-09 16:45:27 +0800

    little change

--- models/mamba.py
+++ models/mamba.py
@@ -1,1 +1,15 @@
-import zeta
+from torch import nn, tensor
+from zeta import SSM
+
+model= SSM()
+
+class MambaBlock(nn.Module):
+    def __int__(
+        self,
+        dim: int,
+        in_features: int,
+        dt_rank: int,
+        dim_inner: int,
+        d_state: int
+
+    ):
